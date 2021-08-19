3247f61b021fbbc8ede6273fceb19ad36fdbd1e9
Luis Carlos de Souza Muniz <luisc.souzamuniz@gmail.com>
2021-08-18 21:43:45 -0300

    feat(frontend): adicionado view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,430 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+  <meta name="copyright" content="MACode ID, https://www.macodeid.com/">
+  
+  <title>Valandil :: Portfolio</title>
+
+  <link rel="shortcut icon" href="site/assets/favicon.ico" type="image/x-icon">
+  
+  <link rel="stylesheet" type="text/css" href="site/assets/css/themify-icons.css">
+  
+  <link rel="stylesheet" type="text/css" href="site/assets/css/bootstrap.css">
+
+  <link rel="stylesheet" type="text/css" href="site/assets/vendor/animate/animate.css">
+  
+  <link rel="stylesheet" type="text/css" href="site/assets/vendor/owl-carousel/owl.carousel.css">
+  
+  <link rel="stylesheet" type="text/css" href="site/assets/vendor/perfect-scrollbar/css/perfect-scrollbar.css">
+  
+  <link rel="stylesheet" type="text/css" href="site/assets/vendor/nice-select/css/nice-select.css">
+  
+  <link rel="stylesheet" type="text/css" href="site/assets/vendor/fancybox/css/jquery.fancybox.min.css">
+  
+  <link rel="stylesheet" type="text/css" href="site/assets/css/virtual.css">
+  
+  <link rel="stylesheet" type="text/css" href="site/assets/css/topbar.virtual.css">
+</head>
+<body class="theme-red">
+  
+  <!-- Back to top button -->
+  <div class="btn-back_to_top">
+    <span class="ti-arrow-up"></span>
+  </div>
+  
+  <!-- Setting button -->
+  <div class="config">
+    <div class="template-config">
+      <!-- Settings -->
+      <div class="d-block">
+        <button class="btn btn-fab btn-sm" id="sideel" title="Settings"><span class="ti-settings"></span></button>
+      </div>
+      <!-- Puschase -->
+      <div class="d-block">
+        <a href="https://macodeid.com/projects/virtual-folio/" class="btn btn-fab btn-sm" title="Get this template" data-toggle="tooltip" data-placement="left"><span class="ti-download"></span></a>
+      </div>
+      <!-- Help -->
+      <div class="d-block">
+        <a href="#" class="btn btn-fab btn-sm" title="Help" data-toggle="tooltip" data-placement="left"><span class="ti-help"></span></a>
+      </div>
+    </div>
+  </div>
+  
+  <div class="vg-page page-home" id="home" style="background-image: url(site/assets/img/bg_image_1.jpg)">
+    <!-- Navbar -->
+    <div class="navbar navbar-expand-lg navbar-dark sticky" data-offset="500">
+      <div class="container">
+        <a href="" class="navbar-brand">Valandil</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#main-navbar" aria-expanded="true">
+          <span class="ti-menu"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-navbar">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a href="#home" class="nav-link" data-animate="scrolling">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="#about" class="nav-link" data-animate="scrolling">Sobre</a>
+            </li>
+            <li class="nav-item">
+              <a href="#portfolio" class="nav-link" data-animate="scrolling">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a href="#contact" class="nav-link" data-animate="scrolling">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div> <!-- End Navbar -->
+    <!-- Caption header -->
+    <div class="caption-header text-center wow zoomInDown">
+      <h5 class="fw-normal">Bem vindo</h5>
+      <h1 class="fw-light mb-4">Eu sou <b class="fg-theme">Luis Carlos de Souza</b></h1>
+      <div class="badge">Fullstack developer (Ruby, ReactJS)</div>
+    </div> <!-- End Caption header -->
+    <div class="floating-button"><span class="ti-mouse"></span></div>
+  </div>
+  
+  <div class="vg-page page-about" id="about">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-lg-4 py-3">
+          <div class="img-place wow fadeInUp">
+            <img src="site/assets/img/perfil.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-lg-6 offset-lg-1 wow fadeInRight">
+          <h1 class="fw-light">Luis Carlos de Souza</h1>
+          <h5 class="fg-theme mb-3">Fullstack developer</h5>
+          <p class="text-muted">Formado em Análise e Desenvolvimento de Sistemas (2017). Pós graduado em Engenharia de Software (2021)</p>
+          <ul class="theme-list">
+            <li>Sou de Santos, São Paulo </li>
+            <li>Moro em Blumenau, Santa Catarina</li>
+            <li>Tenho 26 anos</li>
+          </ul>
+          <button class="btn btn-theme-outline">Download CV</button>
+        </div>
+      </div>
+    </div>
+    <div class="container py-5">
+      <h1 class="text-center fw-normal wow fadeIn">Habilidades</h1>
+      <div class="row py-3">
+        <div class="col-md-6">
+          <div class="px-lg-3">
+            <h4 class="wow fadeInUp">Programação</h4>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">JavaScript</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 86%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">86%</div>
+              </div>
+            </div>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">Ruby</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">80%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="px-lg-3">
+            <h4 class="wow fadeInUp">Web Design</h4>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">CSS</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 92%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">92%</div>
+              </div>
+            </div>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">HTML</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 99%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">99%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container pt-5">
+      <div class="row">
+        <div class="col-md-6 wow fadeInRight">
+          <h2 class="fw-normal">Formação acadêmica</h2>
+          <ul class="timeline mt-4 pr-md-5">
+            <li>
+              <div class="title">2021</div>
+              <div class="details">
+                <h5>Pós graduação</h5>
+                <small class="fg-theme">Universidade Norte do Paraná</small>
+                <p>
+                  Engenharia de software
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="title">2020</div>
+              <div class="details">
+                <h5>Pós graduação</h5>
+                <small class="fg-theme">Universidade Cruzeiro do Sul</small>
+                <p>
+                  Tecnologia e Inovação na Educação
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="title">2017</div>
+              <div class="details">
+                <h5>Graduação</h5>
+                <small class="fg-theme">Centro Universitário do Vale do Ribeira</small>
+                <p>
+                  Análise e Desenvolvimento de Sistemas
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-6 wow fadeInRight" data-wow-delay="200ms">
+          <h2 class="fw-normal">Experiência</h2>
+          <ul class="timeline mt-4 pr-md-5">
+            <li>
+              <div class="title">2019 - Atual</div>
+              <div class="details">
+                <h5>Gett</h5>
+                <small class="fg-theme">Fullstack Developer</small>
+                <p>
+                  Ruby on Rails, ReactJS, GraphQL e microservices
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="title">2016 - 2018</div>
+              <div class="details">
+                <h5>Centro Universitário do Vale do Ribeira</h5>
+                <small class="fg-theme">Programador Fullstack</small>
+                <p>
+                  PHP e Javascript
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="title">2015</div>
+              <div class="details">
+                <h5>SIGRB</h5>
+                <small class="fg-theme">Estágiario</small>
+                <p>
+                  PHP e Javascript
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="vg-page page-service">
+    <div class="container">
+      <div class="text-center wow fadeInUp">
+        <div class="badge badge-subhead">Serviços</div>
+      </div>
+      <h1 class="fw-normal text-center wow fadeInUp">O que posso fazer?</h1>
+      <div class="row mt-5">
+        <div class="col-md-6 col-lg-4 col-xl-3">
+          <div class="card card-service wow fadeInUp">
+            <div class="icon">
+              <span class="ti-desktop"></span>
+            </div>
+            <div class="caption">
+              <h4 class="fg-theme">Desenvolvimento Web</h4>
+              <p>
+                Desenvolvimento de sites e aplicações web
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3">
+          <div class="card card-service wow fadeInUp">
+            <div class="icon">
+              <span class="ti-vector"></span>
+            </div>
+            <div class="caption">
+              <h4 class="fg-theme">Integrações</h4>
+              <p>
+                Criação e integração de API's REST ou GraphQL
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3">
+          <div class="card card-service wow fadeInUp">
+            <div class="icon">
+              <span class="ti-panel"></span>
+            </div>
+            <div class="caption">
+              <h4 class="fg-theme">Bot para discord</h4>
+              <p>
+                Bot de interação e integrado com outras plataformas
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3">
+          <div class="card card-service wow fadeInUp">
+            <div class="icon">
+              <span class="ti-dashboard"></span>
+            </div>
+            <div class="caption">
+              <h4 class="fg-theme">Dashboards</h4>
+              <p>
+                Desenvolvimento de dashboards
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Portfolio page -->
+  <div class="vg-page page-portfolio" id="portfolio">
+    <div class="container">
+      <div class="text-center wow fadeInUp">
+        <div class="badge badge-subhead">Portfolio</div>
+      </div>
+      <h1 class="text-center fw-normal wow fadeInUp">Projetos</h1>
+
+      <div class="gridder my-3">
+        <div class="grid-item apps wow zoomIn">
+          <div class="img-place" data-src="site/assets/img/work/work-1.jpg" data-fancybox data-caption="<h5 class='fg-theme'>Mobile Travel App</h5> <p>Travel, Discovery</p>">
+            <img src="site/assets/img/work/work-1.jpg" alt="">
+            <div class="img-caption">
+              <h5 class="fg-theme">PokeTeste</h5>
+              <p>Lista de pokemons</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-item template wireframes wow zoomIn">
+          <div class="img-place" data-src="site/assets/img/poketeste.png" data-fancybox data-caption="<h5 class='fg-theme'>API</h5>">
+            <img src="site/assets/img/poketeste.png" alt="">
+            <div class="img-caption">
+              <h5 class="fg-theme">Site integrado com Pokemon API</h5>
+              <p>ReactJS</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-item apps ios wow zoomIn">
+          <div class="img-place" data-src="site/assets/img/work/work-3.jpg" data-fancybox data-caption="<h5 class='fg-theme'>Gaming Dashboard</h5> <p>Games, Streaming</p>">
+            <img src="site/assets/img/work/work-3.jpg" alt="">
+            <div class="img-caption">
+              <h5 class="fg-theme">Gaming Dashboard</h5>
+              <p>Games, Streaming</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-item graphic ui-ux wow zoomIn">
+          <div class="img-place" data-src="site/assets/img/work/work-4.jpg" data-fancybox data-caption="<h5 class='fg-theme'>Drugs Delivery App</h5> <p>Health, Drugs</p>">
+            <img src="site/assets/img/work/work-4.jpg" alt="">
+            <div class="img-caption">
+              <h5 class="fg-theme">Drugs Delivery App</h5>
+              <p>Health, Drugs</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-item apps ios wow zoomIn">
+          <div class="img-place" data-src="site/assets/img/work/work-5.jpg" data-fancybox data-caption="<h5 class='fg-theme'>Musics Discover</h5> <p>Musics, Dashboard</p>">
+            <img src="site/assets/img/work/work-5.jpg" alt="">
+            <div class="img-caption">
+              <h5 class="fg-theme">Musics Discover</h5>
+              <p>Musics, Dashboard</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-item graphic ui-ux wireframes wow zoomIn">
+          <div class="img-place" data-src="site/assets/img/work/work-6.jpg" data-fancybox data-caption="<h5 class='fg-theme'>Game Streaming</h5> <p>Games, Streaming</p>">
+            <img src="site/assets/img/work/work-6.jpg" alt="">
+            <div class="img-caption">
+              <h5 class="fg-theme">Game Streaming</h5>
+              <p>Games, Streaming</p>
+            </div>
+          </div>
+        </div>
+      </div> <!-- End gridder -->
+    </div>
+  </div> <!-- End Portfolio page -->
+  
+  <!-- Contact -->
+  <div class="vg-page page-contact" id="contact">
+    <div class="container-fluid">
+      <div class="text-center wow fadeInUp">
+        <div class="badge badge-subhead">Contato</div>
+      </div>
+      <h1 class="text-center fw-normal wow fadeInUp">Entre em contato</h1>
+      <div class="row py-5">
+        <div class="col-lg-12">
+          <form class="vg-contact-form">
+            <div class="form-row">
+              <div class="col-12 mt-3 wow fadeInUp">
+                <input class="form-control" type="text" name="Name" placeholder="Nome">
+              </div>
+              <div class="col-6 mt-3 wow fadeInUp">
+                <input class="form-control" type="text" name="Email" placeholder="Email">
+              </div>
+              <div class="col-6 mt-3 wow fadeInUp">
+                <input class="form-control" type="text" name="Subject" placeholder="Assunto">
+              </div>
+              <div class="col-12 mt-3 wow fadeInUp">
+                <textarea class="form-control" name="Message" rows="6" placeholder="Digite uma mensagem.."></textarea>
+              </div>
+              <button type="submit" class="btn btn-theme mt-3 wow fadeInUp ml-1">Enviar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div> <!-- End Contact -->
+  
+  <!-- Footer -->
+  <div class="vg-footer">
+    <h1 class="text-center">Valandil</h1>
+    <div class="container">
+      <div class="row justify-content-center mt-3">
+        <div class="col-12">
+          <p class="text-center mb-0 mt-4">Copyright &copy;2021. Todos os direitos reservados.</p>
+        </div>
+      </div>
+    </div>
+  </div> <!-- End footer -->
+  
+  
+  <script src="site/assets/js/jquery-3.5.1.min.js"></script>
+    
+  <script src="site/assets/js/bootstrap.bundle.min.js"></script>
+    
+  <script src="site/assets/vendor/owl-carousel/owl.carousel.min.js"></script>
+    
+  <script src="site/assets/vendor/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+    
+  <script src="site/assets/vendor/isotope/isotope.pkgd.min.js"></script>
+    
+  <script src="site/assets/vendor/nice-select/js/jquery.nice-select.min.js"></script>
+    
+  <script src="site/assets/vendor/fancybox/js/jquery.fancybox.min.js"></script>
+
+  <script src="site/assets/vendor/wow/wow.min.js"></script>
+
+  <script src="site/assets/vendor/animateNumber/jquery.animateNumber.min.js"></script>
+
+  <script src="site/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+
+  <script src="site/assets/js/google-maps.js"></script>
+    
+  <script src="site/assets/js/topbar-virtual.js"></script>
+
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIA_zqjFMsJM_sxP9-6Pde5vVCTyJmUHM&callback=initMap"></script>
+  
+</body>
+</html>

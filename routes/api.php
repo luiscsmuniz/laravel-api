25ee31ae022eee80e552027c32f0fc73b7be40df
Luis Carlos de Souza <luisc.souzamuniz@gmail.com>
2021-08-20 22:49:07 +0000

    feat(laravel-api): ajustes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\api\PhraseController;
 
-Route::apiResource('phrases', 'App\Http\Controllers\api\PhraseController');
-
 Route::get('/random-phrase', [PhraseController::class, 'random'] );
+Route::get('/phrase/{id}', [PhraseController::class, 'show'] );
+Route::get('/phrases', [PhraseController::class, 'index'] );
+Route::post('/create-phrase', [PhraseController::class, 'store'] );
+Route::put('/update-phrase/{id}', [PhraseController::class, 'update'] );
+Route::delete('/delete-phrase/{id}', [PhraseController::class, 'destroy'] );
